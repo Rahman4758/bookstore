867654f0f7d602026182283ebf357db14fdb1bd9
Mohammad Rahman <93023714+Rahman4758@users.noreply.github.com>
2024-10-02 16:42:23 +0530

    Update Signup.jsx

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -21,7 +21,7 @@ function Signup() {
       password: data.password,
     };
     await axios
-      .post("http://localhost:4001/user/Signup", userInfo)
+      .post("https://bookstore-backend-ten.vercel.app/Signup", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
